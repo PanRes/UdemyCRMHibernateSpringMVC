00c776d2878b17d80082d75e53befb4ed48a0be4
PR <p.ressos@hotmail.com>
2018-04-30 15:48:14 +0300

    sql scripts added

---
+++ sql/udemy_crm_customer.sql
@@ -0,0 +1,3 @@
+INSERT INTO udemy_crm.customer (id, first_name, last_name, email) VALUES (1, 'Panagiotis', 'Ressos', 'p.ressos@hotmail.com');
+INSERT INTO udemy_crm.customer (id, first_name, last_name, email) VALUES (2, 'Makis', 'Kotsovos', 'mccoy@gmail.com');
+INSERT INTO udemy_crm.customer (id, first_name, last_name, email) VALUES (3, 'Aggelos', 'Perlianis', 'angel@gmail.com');
---
+++ sql/udemy_crm_customer_updates.sql
@@ -0,0 +1,3 @@
+UPDATE udemy_crm.customer SET first_name = 'Panagiotis', last_name = 'Ressos', email = 'p.ressos@hotmail.com' WHERE id = 1;
+UPDATE udemy_crm.customer SET first_name = 'Makis', last_name = 'Kotsovos', email = 'mccoy@gmail.com' WHERE id = 2;
+UPDATE udemy_crm.customer SET first_name = 'Aggelos', last_name = 'Perlianis', email = 'angel@gmail.com' WHERE id = 3;
