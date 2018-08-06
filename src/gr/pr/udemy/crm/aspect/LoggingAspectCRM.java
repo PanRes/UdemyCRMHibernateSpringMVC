2701032ef82c79f6746d09c6f2fa769e1ee9a689
pressos <p.ressos@hotmail.com>
2018-08-06 18:15:44 +0300

    Section 45 Lecture 383 aop added( failed to deploy)

---
+++ src/gr/pr/udemy/crm/aspect/LoggingAspectCRM.java
@@ -0,0 +1,37 @@
+package gr.pr.udemy.crm.aspect;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class LoggingAspectCRM {
+	Logger logger = Logger.getLogger(getClass().getName());
+	
+	@Pointcut("execution(* gr.pr.udemy.crm.controllers.*.*(..))")
+	private void forControllerPackage() {}
+	
+	@Pointcut("execution(* gr.pr.udemy.crm.services.*.*(..))")
+	private void forServicePackage() {}
+	
+	@Pointcut("execution(* gr.pr.udemy.crm.dao.*.*(..))")
+	private void forDaoPackage() {}
+	
+	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+	private void forAppFlow() {}
+	
+	@Before("forAppFlow()")
+	private void before(JoinPoint joinPoint) {
+		
+		String method = joinPoint.getSignature().toShortString();
+		logger.info("====> in @Before: calling method: " + method);
+		
+		Object[] args = joinPoint.getArgs();
+	}
+	
+}
